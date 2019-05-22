76695ac06aefe30e023f2f13e1eb5b2703f9cbe8
Conner Theberge <ctheberge96@gmail.com>
2019-05-21 22:54:08 -0400

    Working Towards New Tasks

---
+++ SimpleStraw/Assets/Scripts/Container.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+
+    private List<GameObject> _contents;
+    public int capacity;
+
+    public bool isFull {
+        get {
+            return _contents.Count >= capacity;
+        }
+    }
+
+    public bool Contains(GameObject item) {
+
+        foreach (GameObject obj in _contents) {
+
+            return obj == item;
+
+        }
+
+        return false;
+    
+    }
+
+    public bool ContainsType(System.Type type) {
+    
+        foreach (GameObject obj in _contents) {
+
+            return obj.GetComponent(type) != null;
+
+        }
+
+        return false;
+    
+    }
+
+    public bool Add(GameObject item) {
+
+        if (!isFull && !_contents.Contains(item)) {
+
+            _contents.Add(item);
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public GameObject Take(GameObject item) {
+
+        if (_contents.Contains(item)) {
+
+            _contents.Remove(item);
+
+            return item;
+
+        }
+
+        return null;
+
+    }
+
+    public GameObject TakeType(System.Type type) {
+
+        foreach (GameObject obj in _contents) {
+
+            if (obj.GetComponent(type) != null) {
+
+                _contents.Remove(obj);
+                return obj;
+
+            }
+
+        }
+
+        return null;
+        
+    }
+
+    void Start() {
+
+        _contents = new List<GameObject>();
+
+    }
+
+}
--- SimpleStraw/Assets/Scripts/WorldManager.cs
+++ SimpleStraw/Assets/Scripts/WorldManager.cs
@@ -9,4 +9,24 @@ public class WorldManager
             return GameObject.Find("WorldManager").GetComponent<Grid>();
         }
     }
+
+    public static GameObject CreateManifestedObject(GameObject source, Vector3 position) {
+
+        if (source == null || position == null) { return null; }
+
+        GameObject obj = GameObject.Instantiate(source, position, Quaternion.identity);
+
+        Manifest.Register(obj);
+
+        return obj;
+
+    }
+
+    public static void DestroyManifestedObject(GameObject obj) {
+
+        Manifest.DeRegister(obj);
+        GameObject.Destroy(obj);
+
+    }
+
 }
